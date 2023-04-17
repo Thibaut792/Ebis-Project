7f83792d6a461e654606e8207b3a18dd5d8b474d
Your Name <BeNyEz17>
2023-04-17 09:26:58 +0200

    Ajoutez des fichiers projet.

---
+++ Ebis/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ebis;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Ebis/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ebis;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
